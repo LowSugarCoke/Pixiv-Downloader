c0da8621f10c57ab5f447db82a5294c858e1d009
Jack Lee <jack20499@gmail.com>
2022-03-07 23:44:49 +0800

    delete log in

--- crawlpixiv.py
+++ crawlpixiv.py
@@ -10,21 +10,6 @@ option.add_experimental_option("detach",True)
 driver = webdriver.Chrome(options=option)
 driver.get('https://www.pixiv.net/ranking.php/') 
 
-# member_button = driver.find_element_by_xpath('/html/body/div[2]/div/section/div[2]/div/a[2]')
-# member_button.click()
-
-# acount = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/form/div[1]/div[1]/input')
-# acount.send_keys('jack20499@yahoo.com.tw')
-
-# password = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/form/div[1]/div[2]/input')
-# password.send_keys('7SgfqAPfH3TetuM')
-
-# submit = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/form/button')
-# submit.click()
-
-# time.sleep(3)
-
-
 ua = UserAgent()
 count = 0 
 picpath = './data/'
