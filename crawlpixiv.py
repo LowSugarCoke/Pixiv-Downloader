5b1449f78203b69cd5f0877c13e79435797716b6
Jack Lee <jack20499@gmail.com>
2022-03-09 22:03:35 +0800

    add rule of file name and folder name

--- crawlpixiv.py
+++ crawlpixiv.py
@@ -3,34 +3,56 @@ import requests
 import time
 import re
 from fake_useragent import UserAgent
+import os
 
+#turn on page
 option = webdriver.ChromeOptions()
 option.add_experimental_option("detach",True)
-
 driver = webdriver.Chrome(options=option)
 driver.get('https://www.pixiv.net/ranking.php/') 
 
-ua = UserAgent()
-count = 0 
-picpath = './data/'
+#Get date
+date = re.findall(r'\[\w+\.\w+\.\w+\]',driver.page_source)
+date = date[0]
+date = date.strip('[').strip(']')
+print(date)
 
+os.makedirs("./data/"+date, exist_ok=True)
+
+# ua = UserAgent()
+count = 0 
+picpath = './data/'+date
+
+#Get image name
+name = re.findall(r'"noopener">.*?>',driver.page_source)
+name=name[1:51]
+for i in range(0,len(name)):
+    name[i] = name[i].strip('"noopener">').strip('</a>')
+print(name)
+
+
+# Get image url
 info = re.findall(r'/artworks/\d*',driver.page_source)
 info = info[::2]
 for url in info:
     print(url)
     print("http://www.pixiv.net"+url)
-    r = requests.get("http://www.pixiv.net"+url)
     
+    #Get big size image url
+    r = requests.get("http://www.pixiv.net"+url)
     u = re.findall(r'https://i.pximg.net/img-original/img.*?g',r.text)
     
     print(u)
     print(u[0])
     try:
-        filename = picpath+"/" + str(count) + ".jpg"       
+        filename = picpath+"/" + "#"+str(count+1)+" "+str(name[count]) + ".jpg"       
 
+        #fake agent
         headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',            
                    'referer':'http://www.pixiv.net%s' % str(url)
-                   }     
+                   }   
+        
+        #download image  
         r = requests.get(u[0],headers = headers)        
         with open(filename, 'wb') as f:
             f.write(r.content)
